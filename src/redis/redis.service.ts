ef42be5d8208ed3f80248438c81ca8c800647d73
jakub barczewski <jakub.barczewski@netguru.com>
2022-09-17 18:43:34 +0200

    feat(redis.service.ts): add basic get and set functionalities

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@nestjs/common';
-import { Redis } from 'ioredis';
+import Redis, { RedisKey } from 'ioredis';
+
+type RedisValue = string | number | Buffer;
 
 @Injectable()
 export class RedisService {
   private client: Redis;
+  constructor() {
+    this.client = new Redis();
+  }
 
-  setClient(client: Redis) {
+  public setClient(client: Redis) {
     this.client = client;
   }
 
-  getClient() {
+  public getClient() {
     return this.client;
+  }
+
+  public async get(key: RedisKey): Promise<string | null> {
+    return this.client.get(key);
+  }
+
+  public async set(key: RedisKey, value: RedisValue): Promise<'OK'> {
+    return this.client.set(key, value);
   }
 }
